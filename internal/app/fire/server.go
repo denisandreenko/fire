722465b525a2479042c68f496eb9436e711c4154
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-01-29 12:51:29 +0400

    add repo configuration

--- internal/app/fire/server.go
+++ internal/app/fire/server.go
@@ -4,15 +4,17 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/denisandreenko/fire/internal/app/repository"
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
 // Server ...
 type Server struct {
-	config *Config
-	logger *logrus.Logger
-	router *mux.Router
+	config     *Config
+	logger     *logrus.Logger
+	router     *mux.Router
+	repository *repository.Repository
 }
 
 func New(config *Config) *Server {
@@ -31,6 +33,10 @@ func (s *Server) Start() error {
 
 	s.configureRouter()
 
+	if err := s.configureRepository(); err != nil {
+		return err
+	}
+
 	s.logger.Info("starting server")
 
 	return http.ListenAndServe(s.config.Service.BindAddr, s.router)
@@ -48,10 +54,21 @@ func (s *Server) configureLogger() error {
 }
 
 func (s *Server) configureRouter() {
-	s.router.HandleFunc("/ishealthy", s.isHealthy())
+	s.router.HandleFunc("/ishealthy", s.handleIsHealthy())
 }
 
-func (s *Server) isHealthy() http.HandlerFunc {
+func (s *Server) configureRepository() error {
+	repo := repository.New(s.config.Repository)
+	if err := repo.Open(); err != nil {
+		return err
+	}
+
+	s.repository = repo
+
+	return nil
+}
+
+func (s *Server) handleIsHealthy() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "healthy")
 	}
