5ca775ddd5010102aa73d6bbc4decd382ae30272
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-02-09 13:41:53 +0100

    add server unit tests

--- internal/app/fire/server_test.go
+++ internal/app/fire/server_test.go
@@ -5,16 +5,23 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/denisandreenko/fire/internal/app/store/teststore"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAPIServer_HandleHello(t *testing.T) {
-	testConfig := &Config{}
-	testConfig.Service.BindAddr = ":8080"
+func TestServer_HandleUsersCreate(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	s := newServer(teststore.New())
+	s.ServeHTTP(rec, req)
+	assert.Equal(t, rec.Code, http.StatusOK)
+}
 
-	s := New(testConfig)
+func TestAPIServer_HandleHello(t *testing.T) {
 	rec := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "/ishealthy", nil)
-	s.handleIsHealthy().ServeHTTP(rec, req)
+	s := newServer(teststore.New())
+	s.ServeHTTP(rec, req)
+	assert.Equal(t, rec.Code, http.StatusOK)
 	assert.Equal(t, rec.Body.String(), "healthy")
 }
