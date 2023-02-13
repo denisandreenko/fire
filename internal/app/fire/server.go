56d1d573e08ddae0caa4b2aa28841e0bc7649626
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-02-13 21:55:37 +0100

    handle users creation

--- internal/app/fire/server.go
+++ internal/app/fire/server.go
@@ -1,24 +1,23 @@
 package fire
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 
+	"github.com/denisandreenko/fire/internal/app/model"
 	"github.com/denisandreenko/fire/internal/app/store"
 	"github.com/gorilla/mux"
-	"github.com/sirupsen/logrus"
 )
 
 type server struct {
 	router *mux.Router
-	logger *logrus.Logger
 	store  store.Store
 }
 
 func newServer(store store.Store) *server {
 	s := &server{
 		router: mux.NewRouter(),
-		logger: logrus.New(),
 		store:  store,
 	}
 
@@ -44,7 +43,39 @@ func (s *server) handleIsHealthy() http.HandlerFunc {
 }
 
 func (s *server) handleUsersCreate() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
 
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u := &model.User{
+			Email:    req.Email,
+			Password: req.Password,
+		}
+		if err := s.store.User().Create(u); err != nil {
+			s.error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		u.Sanitize()
+		s.respond(w, r, http.StatusCreated, u)
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
 	}
 }
