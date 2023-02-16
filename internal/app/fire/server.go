6d3ff7a4a2d4f0e6877b8d8d0e591ac0413a003d
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-02-16 15:24:32 +0100

    add create seesion handler

--- internal/app/fire/server.go
+++ internal/app/fire/server.go
@@ -8,17 +8,24 @@ import (
 	"github.com/denisandreenko/fire/internal/app/model"
 	"github.com/denisandreenko/fire/internal/app/store"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+)
+
+const (
+	sessionName = "session"
 )
 
 type server struct {
-	router *mux.Router
-	store  store.Store
+	router       *mux.Router
+	store        store.Store
+	sessionStore sessions.Store
 }
 
-func newServer(store store.Store) *server {
+func newServer(store store.Store, sessionStore sessions.Store) *server {
 	s := &server{
-		router: mux.NewRouter(),
-		store:  store,
+		router:       mux.NewRouter(),
+		store:        store,
+		sessionStore: sessionStore,
 	}
 
 	s.configureRouter()
@@ -34,6 +41,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (s *server) configureRouter() {
 	s.router.HandleFunc("/ishealthy", s.handleIsHealthy()).Methods("GET")
 	s.router.HandleFunc("/users", s.handleUsersCreate()).Methods("POST")
+	s.router.HandleFunc("/sessions", s.handleSessionsCreate()).Methods("POST")
 }
 
 func (s *server) handleIsHealthy() http.HandlerFunc {
@@ -66,6 +74,41 @@ func (s *server) handleUsersCreate() http.HandlerFunc {
 
 		u.Sanitize()
 		s.respond(w, r, http.StatusCreated, u)
+	}
+}
+
+func (s *server) handleSessionsCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u, err := s.store.User().FindByEmail(req.Email)
+		if err != nil || !u.ComparePassword(req.Password) {
+			s.error(w, r, http.StatusUnauthorized, store.ErrIncorrectEmailOrPassword)
+			return
+		}
+
+		session, err := s.sessionStore.Get(r, sessionName)
+		if err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		session.Values["user_id"] = u.ID
+		if err := s.sessionStore.Save(r, w, session); err != nil {
+			s.error(w, r, http.StatusInternalServerError, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, nil)
 	}
 }
 
