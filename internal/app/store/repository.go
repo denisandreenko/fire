65126372757bc60cd8af83bf7f56e3e0b234382e
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-02-19 20:46:08 +0100

    add find user ipml

--- internal/app/store/repository.go
+++ internal/app/store/repository.go
@@ -5,5 +5,6 @@ import "github.com/denisandreenko/fire/internal/app/model"
 // UserRepository ...
 type UserRepository interface {
 	Create(*model.User) error
+	Find(int) (*model.User, error)
 	FindByEmail(string) (*model.User, error)
 }
