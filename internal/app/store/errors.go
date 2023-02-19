d79a79aeb9773a92a8008a82bea0545cceca9f78
Dzianis Andreyenka <andreenkodn@gmail.com>
2023-02-19 20:42:04 +0100

    add new error type

--- internal/app/store/errors.go
+++ internal/app/store/errors.go
@@ -5,6 +5,8 @@ import "errors"
 var (
 	// ErrRecordNotFound ...
 	ErrRecordNotFound = errors.New("record not found")
-	// ErrIncorrectEmailOrPassword
+	// ErrIncorrectEmailOrPassword ...
 	ErrIncorrectEmailOrPassword = errors.New("incorrect email or password")
+	// ErrNotAuthenticated ...
+	ErrNotAuthenticated = errors.New("not authenticated")
 )
